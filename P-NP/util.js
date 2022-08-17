8ebd53b523b8b5145c22f5391c298ba520161392
donaldli2020 <74021676+donaldli2020@users.noreply.github.com>
2022-08-16 23:24:03 -0400

    fix headache part 1

--- P-NP/util.js
+++ P-NP/util.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPatchedPublicGameFile = exports.getPatchedGameFile = exports.patchGameFile = exports.logtraffic = exports.getGameFile = exports.getGameStatus = void 0;
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const constants_1 = require("./constants");
-const displayImages_1 = require("./displayImages");
 const sucrase_1 = require("sucrase");
 const es6 = (...args) => (0, sucrase_1.transform)(String.raw(...args), { transforms: ["typescript"] }).code;
 // insert your own developer cheat menu here, if not it'll default to WCM
@@ -50,76 +49,17 @@ exports.getGameFile = getGameFile;
 const patchGameFile = (str, version) => {
     const variables = [str.match(/window,function\((.)/)[1], str.match(/var (.)={}/)[1]];
     const patches = Object.entries({
-        [`s),this._game=${variables[1]}`]: `s),this._game=${variables[1]};
-			jQuery.temp = window._;
-			let lodashChecker = setInterval(() => {
-				if (jQuery.temp !== window._) {
-					window._ = jQuery.temp;
-					delete jQuery.temp;
-					clearInterval(lodashChecker);
-				}
-			});
-			Object.defineProperty(window._, "instance", { 
-				get: () => ${variables[0]}.instance,
-		enumerable: true,
-	configurable: true
-			});`,
-        [`${variables[0]}.constants=Object`]: `window._.constants=${variables[0]},${variables[0]}.constants=Object`,
-        [`window,function(${variables[0]}){var ${variables[1]}={};`]: `window,function(${variables[0]}){var ${variables[1]}={};window._.modules=${variables[1]};`,
-        [`${variables[0]}.prototype.hasMembership=`]: `${variables[1]}.prototype.hasMembership=_=>true,${variables[1]}.prototype.originalHasMembership=`,
-        "answerQuestion=function(){": `answerQuestion=function(){
-			if (!_.constants.get('GameConstants.Debug.EDUCATION_ENABLED')) {
-				const wasCorrect = Math.random() < _.constants.get('GameConstants.Debug.AUTO_ANSWER_CORRECT_PERCENT');
-                this.onQuestionAnswered.dispatch(wasCorrect, 0, null);
-                if (wasCorrect) {
-                    this.onQuestionAnsweredCorrectly.dispatch(0, null);
-                } else {
-                    this.onQuestionAnsweredIncorrectly.dispatch(0, null);
-                }
-                return;
-			}
+        [`s),this._game=${variables[1]}`]:`s),this._game=${variables[1]};jQuery.temp = window._;let lodashChecker = setInterval(() => {if (jQuery.temp !== window._) {window._ = jQuery.temp;delete jQuery.temp;clearInterval(lodashChecker);}});Object.defineProperty(window._, "instance", { get: () => ${variables[0]}.instance,enumerable: true,configurable: true});`,[`${variables[0]}.constants=Object`]: `window._.constants=${variables[0]},${variables[0]}.constants=Object`,[`window,function(${variables[0]}){var ${variables[1]}={};`]: `window,function(${variables[0]}){var ${variables[1]}={};window._.modules=${variables[1]};`,[`${variables[0]}.prototype.hasMembership=`]: `${variables[1]}.prototype.hasMembership=_=>true,${variables[1]}.prototype.originalHasMembership=`,"answerQuestion=function(){": `answerQuestion=function(){if (!_.constants.get('GameConstants.Debug.EDUCATION_ENABLED')) {const wasCorrect = Math.random() < _.constants.get('GameConstants.Debug.AUTO_ANSWER_CORRECT_PERCENT');this.onQuestionAnswered.dispatch(wasCorrect, 0, null);if (wasCorrect) {this.onQuestionAnsweredCorrectly.dispatch(0, null);} else {this.onQuestionAnsweredIncorrectly.dispatch(0, null);}return;}
 		`
     });
-    patches.push([/type\.sendEvent=function\((.), (.), (.)\) \{/, `type.sendEvent=function($1, $2, $3) {
-			if (!_.constants.get('GameConstants.Debug.EDUCATION_ENABLED')) {
-				return
-			}
+    patches.push([/type\.sendEvent=function\((.), (.), (.)\) \{/, `type.sendEvent=function($1, $2, $3) {if (!_.constants.get('GameConstants.Debug.EDUCATION_ENABLED')) {return}
 		`]);
-    patches.push([/(var .=this.findParameter("externalFactory"))/, `
-	if (!_.constants.get('GameConstants.Debug.EDUCATION_ENABLED')) {
-		const wasCorrect: boolean = Math.random() < _.constants.get('GameConstants.Debug.AUTO_ANSWER_CORRECT_PERCENT');
-		this.finish({ answerCorrect: wasCorrect, responseTime: 0 });
-		return;
-	}
-	$1`]);
-    patches.push([/openQuestionInterfaceThenEmitNotifications=function\((.), (.), (.), (.), (.)\) \{/, `openQuestionInterfaceThenEmitNotifications=function($1, $2, $3, $4, $5) {
-	if (!_.constants.get('GameConstants.Debug.EDUCATION_ENABLED')) {
-		const wasCorrect = true;
-		const skill = {}
-		const questionAnswerResponse = { eventType, skill, wasCorrect };
-		this.fireEvent(MathTowerNotificationType.TOWER_TOWN_QUESTION_ANSWERED, questionAnswerResponse);
-		if (callback) {
-			callback(wasCorrect, 10, 1, false, false, skill);
-		}
-		return;
-	}
+    patches.push([/(var .=this.findParameter("externalFactory"))/, `if (!_.constants.get('GameConstants.Debug.EDUCATION_ENABLED')) {const wasCorrect: boolean = Math.random() < _.constants.get('GameConstants.Debug.AUTO_ANSWER_CORRECT_PERCENT');this.finish({ answerCorrect: wasCorrect, responseTime: 0 });return;}$1
+		`]);
+    patches.push([/openQuestionInterfaceThenEmitNotifications=function\((.), (.), (.), (.), (.)\) \{/, `openQuestionInterfaceThenEmitNotifications=function($1, $2, $3, $4, $5) {if (!_.constants.get('GameConstants.Debug.EDUCATION_ENABLED')) {const wasCorrect = true;const skill = {}const questionAnswerResponse = { eventType, skill, wasCorrect };this.fireEvent(MathTowerNotificationType.TOWER_TOWN_QUESTION_ANSWERED, questionAnswerResponse);if (callback) {callback(wasCorrect, 10, 1, false, false, skill);}return;}
 	`]);
     patches.push([/.\.setContentVisible\(!1\)\}\)/, "})"]);
-    return `
-${es6 `
-	/** DO NOT TOUCH **/
-	const _getBox=(o,t)=>({string:"+",style:"font-size: 1px; padding: 0 "+Math.floor(o/2)+"px; line-height: "+t+"px;"});
-	console.image=((o,t=1)=>{const e=new Image;e.onload=(()=>{const n=_getBox(e.width*t,e.height*t);
-	console.log("%c"+n.string,n.style+"background: url("+o+"); background-size: "+e.width*t+"px "
-	+e.height*t+"px; color: transparent;")}),e.src=o});
-	/** ok touch now */
-	const oldLog = console.log.bind(console);
-	console.log = (...d) => {
-		if (d && d.length && typeof d[0] === "string" && d[0].includes("This is a browser feature for developers only")) return "lol no";
-		if (new Error().stack?.split("\n").reverse()[0]?.includes("load-identity")) return "fuck you";
-		return oldLog(...d);
-	};
-	_.variables = Object.create(null);
+    return `${es6 `/** DO NOT TOUCH **/const _getBox=(o,t)=>({string:"+",style:"font-size: 1px; padding: 0 "+Math.floor(o/2)+"px; line-height: "+t+"px;"});console.image=((o,t=1)=>{const e=new Image;e.onload=(()=>{const n=_getBox(e.width*t,e.height*t);console.log("%c"+n.string,n.style+"background: url("+o+"); background-size: "+e.width*t+"px "+e.height*t+"px; color: transparent;")}),e.src=o});/** ok touch now */const oldLog = console.log.bind(console);console.log = (...d) => {if (d && d.length && typeof d[0] === "string" && d[0].includes("This is a browser feature for developers only")) return "lol no";if (new Error().stack?.split("\n").reverse()[0]?.includes("load-identity")) return "fuck you";return oldLog(...d);};_.variables = Object.create(null);
 `}
 
 ${patches.reduce((l, c) => l.replace(...c), str)}
