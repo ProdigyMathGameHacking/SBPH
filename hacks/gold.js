f1f84b88627f9652802d2101a98cc2d160bf2938
gemsvido <gemsvido@gmail.com>
2022-08-29 08:32:49 -0700

    GOLD HACK IS FIXED

--- hacks/gold.js
+++ hacks/gold.js
@@ -6,21 +6,21 @@ function getLevel (lvl) {
     // TODO implement swal laterTM
     const level = lvl;
 
-    if (level.value === undefined) return;
+    if (level === undefined) return;
 
     // now we calculate how many stars the level *should* have
     // from 3-16-1.js:8382
 
 
-    if (level.value === 1) return 0;
+    if (level === 1) return 0;
 
-    const i = level.value - 2;
+    const i = level - 2;
 
     // xpConstant from 3-16-1.js:8528
 
     const xpConstant = 1.042;
     player.data.stars = Math.round((1 - Math.pow(xpConstant, i)) / (1 - xpConstant) * 20 + 10);
-    player.data.level = +level.value;
+    player.data.level = +level;
     player.getLevel = () => {
         return player.data.level;
     };
---
+++ hacks/memberstars.js
@@ -0,0 +1,8 @@
+function getMemberStars (amt) {
+
+    const amount = amt;
+
+    if (amount === undefined) return;
+
+    return player.data.storedMemberStars = amount;
+}
