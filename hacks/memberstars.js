a3c624848f5ac476bead3161e9ad104804be1608
gemsvido <gemsvido@gmail.com>
2022-08-29 08:34:08 -0700

    member stars hack

--- hacks/memberstars.js
+++ hacks/memberstars.js
@@ -1,8 +1,16 @@
 function getMemberStars (amt) {
 
+    /** player variable */
+    const player = Boot.prototype.game._state._current.user.source;
+
+    /** amount of member stars to get */
     const amount = amt;
 
     if (amount === undefined) return;
 
     return player.data.storedMemberStars = amount;
 }
+
+
+// TODO implement swal later
+getMemberStars(100000);
